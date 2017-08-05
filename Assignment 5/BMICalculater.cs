caef40c8a502441d4bd9c976340f49d9711035f1
Imba-lol <best.abuka@gmail.com>
2017-08-04 23:35:17 -0400

    Created BMI Calculator

---
+++ Assignment 5/BMICalculater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Abubakir Myrzaly
+ * ID: 300931945
+ * Date: 8/4/2017
+ * Description: BMI Calculator Project
+ * Version 0.1 - Created the Project
+ */
+
+namespace Assignment_5
+{
+    public partial class BMICalculater : Form
+    {
+        public BMICalculater()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Assignment 5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Abubakir Myrzaly
+ * ID: 300931945
+ * Date: 8/4/2017
+ * Description: BMI Calculator Project
+ * Version 0.1 - Created the Project
+ */
+
+namespace Assignment_5
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new BMICalculater());
+        }
+    }
+}
